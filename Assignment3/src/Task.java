b19bfd5c942eb9dd924f2deacc58f6a7bef656b2
GhantaMownica <60048506+GhantaMownica@users.noreply.github.com>
2020-04-13 20:10:54 -0500

    Sixth Commit

--- Assignment3/src/Task.java
+++ Assignment3/src/Task.java
@@ -1,21 +1,14 @@
 public abstract class Task implements Runnable {
 	String name;
-	boolean isFinished;
+	boolean complete;
 
 	public Task(String name) {
 		this.name = name;
-		this.isFinished = false;
+		this.complete = false;
 	}
 
 	public String getName() {
 		return name;
 	}
-  
-	public boolean getIsFinished() {
-		return isFinished;
-	}
-  
-	public void setIsFinished() {
-		isFinished = true;
-	}
+ 	
 }
