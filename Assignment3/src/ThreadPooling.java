004c8f1c9faa4f7602c84e9b863a7889fee8fc6b
GhantaMownica <60048506+GhantaMownica@users.noreply.github.com>
2020-04-13 20:04:13 -0500

    Fifth Commit

--- Assignment3/src/ThreadPooling.java
+++ Assignment3/src/ThreadPooling.java
@@ -1,90 +1,67 @@
 
 import java.util.ArrayList;
+
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ThreadPooling
 {
- //Thread pool size
- private final int poolS;
 
- //Declare array
- private final InternalTask[] intTasks;
-
- // FIFO ordering
- private boolean isShutdown = false;
-
- private final LinkedBlockingQueue<Task> q;
- private final ArrayList<Task> Tasks;
- //Constructor of the class
  public ThreadPooling(int poolSize){
  this.poolS = poolSize;
- Tasks = new ArrayList<Task>();
  q = new LinkedBlockingQueue<Task>();
- intTasks = new InternalTask[this.poolS];
+ workers = new InternalTask[this.poolS];
  for (int i = 0; i < poolSize; i++) {
- intTasks[i] = new InternalTask("Thread " + i);
- intTasks[i].start();
- }
- }
-
- public void execute(Task t) {
- synchronized (q) {
- q.add(t);
- q.notify();
- }
- }
-
- public void waitForAllTasks() {
- boolean Task1 = true;
- while (Task1) {
- Task1 = false;
- for (Task task : Tasks) {
-	  if (task.getIsFinished() == false) {
-		  Task1 = true;
-	  }
- }
- try {
- Thread.sleep(100);
- } catch (InterruptedException e) {
- System.out.println("Interruption occured: " + e.getMessage());
- }
- }
+	 workers[i] = new InternalTask("Thread " + i);
+	 workers[i].start();
+	 }
  }
 
  public void shutdown() {
- this.isShutdown = true;
+	 this.isShutdown = true;
+     for (int i = 0; i < poolS; i++) {
+         workers[i] = null;
+     }
  }
+ 
+ public void execute(Runnable t) {
+	 synchronized (q) {
+		 q.add((Task) t);
+		 q.notify();
+		 }
+	 }
+
 
  private class InternalTask extends Thread {
- public InternalTask(String s) {
-   super(s);
- }
- @Override
- public void run() {
- Task t;
- while (true) {
- synchronized (q) {
- if (isShutdown && q.isEmpty()) {
- break;
- }
- while (q.isEmpty()) {
- try {
- q.wait();
- } catch (InterruptedException e) {
- System.out.println(" occurred " + e.getMessage());
- }
- }
- t = (Task) q.poll();
- }  
- try {
- t.run();
- //t.setIsFinished();
- t.setIsFinished();
- } catch (RuntimeException e) {
- System.out.println("Thread pool is stopped " + e.getMessage());
- }
- }
- }
-  }
-
+	 
+	 public InternalTask(String s) {
+		 super(s);
+	 }
+	 
+	 public void run() {
+	 Task t;
+	 while (true) {
+		 synchronized (q) {
+			 while (q.isEmpty()) {
+				 try {
+					 q.wait();
+					 } catch (InterruptedException e) {
+						 System.out.println(" occurred " + e.getMessage());
+						 }
+				 }
+			 t = (Task) q.poll();
+		 }
+		 try {
+			 t.run();
+			 } catch (RuntimeException e) {
+				 System.out.println("Thread pool is stopped " + e.getMessage());
+				 }
+		 }
+	 }
+	 }
+ 
+ private final int poolS;
+ private final InternalTask[] workers;
+ private boolean isShutdown = false;
+ private final LinkedBlockingQueue<Task> q;
+ private final ArrayList<Task> Tasks = new ArrayList<Task>();
 }
